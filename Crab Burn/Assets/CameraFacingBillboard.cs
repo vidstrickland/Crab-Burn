08eb38fafd97d064c612e481c0f9a5f81a5bdacd
vid Strickland <vidkid7@gmail.com>
2019-01-26 21:19:58 -0500

    Runner Guy now runs at you!
    Runner Guy will find the player and charge at him.

--- Crab Burn/Assets/CameraFacingBillboard.cs
+++ Crab Burn/Assets/CameraFacingBillboard.cs
@@ -1,42 +1,24 @@
-﻿//cameraFacingBillboard.cs v02
-//by Neil Carter (NCarter)
-//modified by Juan Castaneda (juanelo)
-//
-//added in-between GRP object to perform rotations on
-//added auto-find main camera
-//added un-initialized state, where script will do nothing
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 
 public class CameraFacingBillboard : MonoBehaviour
 {
 
-    public Camera m_Camera;
-    public bool amActive = false;
-    public bool autoInit = false;
-    GameObject myContainer;
+    public Transform crabFinder;
+    GameObject crab;
 
-    void Awake()
+    private void Start()
     {
-        if (autoInit == true)
-        {
-            m_Camera = Camera.main;
-            amActive = true;
-        }
-
-        myContainer = new GameObject();
-        myContainer.name = "GRP_" + transform.gameObject.name;
-        myContainer.transform.position = transform.position;
-        transform.parent = myContainer.transform;
+        crab = GameObject.FindGameObjectWithTag("Player");
+        crabFinder = crab.GetComponent<Transform>();
     }
 
-    //Orient the camera after all movement is completed this frame to avoid jittering
-    void LateUpdate()
+    void Update()
     {
-        if (amActive == true)
+        if(crab != null)
         {
-            myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+            transform.LookAt(crabFinder);
         }
     }
 }
---
+++ Crab Burn/Assets/DudeRunner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeRunner : MonoBehaviour
+{
+    GameObject crab;
+    Vector3 dudePos;
+    public float speed;
+    public float randomizer;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dudePos = this.transform.position;
+        crab = GameObject.FindGameObjectWithTag("Player");
+        randomizer = Random.Range(0f, 0.025f);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PushDude();
+    }
+
+    void PushDude()
+    {
+        rb.AddForce(transform.forward * speed);
+    }
+}
