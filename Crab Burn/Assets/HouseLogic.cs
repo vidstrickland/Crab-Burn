5a1704042587f2539ddd1c7cae910bc617b5d16f
vid Strickland <vidkid7@gmail.com>
2019-01-26 19:35:32 -0500

    Can Now Burn House, Scary Guy
    House now switches to "Burning" state when the crab deals damage to it. Scary Runner has been added and will turn to face you wherever you go.

---
+++ Crab Burn/Assets/HouseLogic.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseLogic : MonoBehaviour {
+
+    public int health;
+    public Texture[] texture;
+    public int currentTexture;
+    bool onFire;
+
+    // Use this for initialization
+    void Start () {
+        health = 1;
+        onFire = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(health <= 0)
+        {
+            ChangeTexture();
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Crab Hand")
+        {
+            health--;
+        }
+    }
+
+    void ChangeTexture()
+    {
+        if (!onFire)
+        {
+            currentTexture++;
+            currentTexture %= texture.Length;
+            GetComponent<Renderer>().material.mainTexture = texture[currentTexture];
+            onFire = true;
+        }
+    }
+}
